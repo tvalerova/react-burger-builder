fac47d580f354dc7138721edddb56500c1a43f58
Tereza Valerova <terezavalerova@gmail.com>
2020-04-06 14:12:49 +0200

    burger.js updated

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
     return (
-        <div></div>
+        <div className='Burger'>
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="bread-bottom" />
+
+        </div>
     );
 };
 
