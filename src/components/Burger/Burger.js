8dcb1300a9cfa8c889e92053eb30e662fa4b9ccc
Tereza Valerova <terezavalerova@gmail.com>
2020-04-06 16:13:29 +0200

    spelling in Burger.css corrected, media query now works

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,15 +3,17 @@ import React from 'react';
 import './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = ( props ) => {
+const burger = (props) => {
     // we change the ingredients from an object to an array, so we can iterate over them
-    let transformedIngredients = Object.keys( props.ingredients )
-        .map( igKey => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
             // we map the object into an array of ingredients
-            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
-                return <BurgerIngredient key={igKey + i} type={igKey} />;
-            } );
-        } )
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key = { igKey + i }
+                type = { igKey }
+                />;
+            });
+        })
         // we flatten the array with reduce - right now we have an array of arrays
         // it receives 2 args - previous and current value
         .reduce((arr, el) => {
@@ -21,18 +23,14 @@ const burger = ( props ) => {
 
     // if no ingredients have been added, we display this message
     if (transformedIngredients.length === 0) {
-        transformedIngredients = <p>Please start adding ingredients!</p>;
+        transformedIngredients = < p > Please start adding ingredients! < /p>;
     }
-    return (
-        <div className='Burger'>
-            <BurgerIngredient type="bread-top" />
-            {transformedIngredients}
-            <BurgerIngredient type="bread-bottom" />
+    return ( 
+        <div className = 'Burger' >
+        <BurgerIngredient type = "bread-top" / > { transformedIngredients } 
+        <BurgerIngredient type = "bread-bottom" / >
         </div>
     );
 };
 
 export default burger;
-
-
-
