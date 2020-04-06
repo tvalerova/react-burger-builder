2a6222a6781bd1dbb956e427d7b036487c5b2e9c
Tereza Valerova <terezavalerova@gmail.com>
2020-04-06 17:11:08 +0200

    css styling added to buildcontrols.css to style the order button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,20 +11,19 @@ const controls = [
 
 const buildControls = (props) => (
     <div className = 'BuildControls'>
-        {/* to only show 2 decimals in the price, we use the toFixed method */}
-        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
-        {controls.map(ctrl => (
-            <BuildControl 
-            key={ctrl.label} 
-            label={ctrl.label} 
-            // we have access to types because we are looping through controls
-            added={() => props.ingredientAdded(ctrl.type)} 
-            // we use the method the property we defined in BurgerBuilder js
-            removed={() => props.ingredientRemoved(ctrl.type)} 
-            disabled={props.disabled[ctrl.type]}
-            />
+    {/* to only show 2 decimals in the price, we use the toFixed method */}
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+    {controls.map(ctrl => (
+        <BuildControl 
+        key={ctrl.label} 
+        label={ctrl.label} 
+        // we have access to types because we are looping through controls
+        added={() => props.ingredientAdded(ctrl.type)} 
+        // we use the method the property we defined in BurgerBuilder js
+        removed={() => props.ingredientRemoved(ctrl.type)} 
+        disabled={props.disabled[ctrl.type]} />
         ))}
-    </div>
-);
-
-export default buildControls;
+        </div>
+        );
+        
+        export default buildControls;
