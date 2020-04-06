5dfb7200cabd756d98e7751eabbfec587fa30f92
Tereza Valerova <terezavalerova@gmail.com>
2020-04-06 18:12:50 +0200

    modal now works but it shows all the time

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 // capital letters - they will be used as global constants
 const INGREDIENT_PRICES = {
@@ -97,6 +99,9 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
+            <Modal>
+                <OderSummary ingredients={this.state.ingredients}/>
+            </Modal>
             <Burger ingredients={this.state.ingredients} />
             <BuildControls 
             ingredientAdded={this.addIngredientHandler}
