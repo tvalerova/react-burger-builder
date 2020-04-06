01840fa48bd5c81237bccd8fa2457e9a0e249734
Tereza Valerova <terezavalerova@gmail.com>
2020-04-06 15:36:50 +0200

    changes make to output ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,16 +1,36 @@
 import React from 'react';
-import './Burger.css';
+
+import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = (props) => {
+const burger = ( props ) => {
+    // we change the ingredients from an object to an array, so we can iterate over them
+    let transformedIngredients = Object.keys( props.ingredients )
+        .map( igKey => {
+            // we map the object into an array of ingredients
+            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            } );
+        } )
+        // we flatten the array with reduce - right now we have an array of arrays
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+    // if no ingredients have been added, we display this message
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
     return (
-        <div className='Burger'>
+        <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
 };
 
 export default burger;
+
+
+
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,14 +5,30 @@ import Burger from '../../components/Burger/Burger';
 
 // this is a class because we want to manage state here
 class BurgerBuilder extends Component {
+    // we could also manage the state this way, but using only state is a more modern syntax
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    
+    state = {
+        ingredients: {
+            salad: 0, 
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+        
+    }
+    
     render () {
         return (
             <Aux>
-                <Burger />
-                <div>Build Controls</div>
+            <Burger ingredients={this.state.ingredients} />
+            <div>Build Controls</div>
             </Aux>
-        );
+            );
+        }
     }
-}
-
-export default BurgerBuilder;
+    
+    export default BurgerBuilder;
