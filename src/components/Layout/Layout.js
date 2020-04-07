2d73305b053d16d29d477d8324ffaeacc292877a
Tereza Valerova <terezavalerova@gmail.com>
2020-04-07 13:54:46 +0200

    Layout.js changed to class-based component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className='Content'>
-            {/* here we will output the component we wrap with the layout */}
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer />
+                <main className='Content'>
+                    {/* here we will output the component we wrap with the layout */}
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+};
 
-export default layout;
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,23 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import styles from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
     return (
-        <div className={styles.SideDrawer}>
-            <div className={styles.Logo}>
-                <Logo />
+        <Aux>
+            {/* backdrop will alway show since show = true */}
+            <Backdrop show/>
+            <div className={styles.SideDrawer}>
+                <div className={styles.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 };
 
