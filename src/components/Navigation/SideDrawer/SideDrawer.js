8716515dac551f739bcc0b199e60cc0452286eed
Tereza Valerova <terezavalerova@gmail.com>
2020-04-07 14:10:03 +0200

    logic added to close sidedrawer on click on the backdrop

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,11 +6,17 @@ import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
+    // we can close the drawer by clicking on the backdrop
+    let attachedClasses = [styles.SideDrawer, styles.Close];
+    if (props.open) {
+        attachedClasses = [styles.SideDrawer, styles.Open];
+    }
+
     return (
         <Aux>
             {/* backdrop will alway show since show = true */}
             <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={styles.SideDrawer}>
+            <div className={attachedClasses.join(' ')}>
                 <div className={styles.Logo}>
                     <Logo />
                 </div>
