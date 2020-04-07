e5d37d938245c2cc05c4d9e1d4daece5d3340ebc
Tereza Valerova <terezavalerova@gmail.com>
2020-04-07 14:34:02 +0200

    logo added instead of MENU

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.jsx
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import './DrawerToggle.css';
+
 
 const drawerToggle = (props) => (
-    <div onClick={props.clicked}>MENU</div>
+    <div className='DrawerToggle' onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 );
 
 export default drawerToggle;
