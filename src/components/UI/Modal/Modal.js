417f4d68ca5c6aecd58cb84e968414a21559e4f1
Tereza Valerova <terezavalerova@gmail.com>
2020-04-07 14:50:39 +0200

    Modal turned into a class-based component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 // the modal will be a component that can be wrapped around any content
-const modal = (props) => (
-    <Aux>
-        {/* if the Modal is shown, the backdrop should show too */}
-        <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div className='Modal'
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component {
 
-export default modal;
+    render() {
+        return (
+            <Aux>
+                {/* if the Modal is shown, the backdrop should show too */}
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div className='Modal'
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
+
+export default Modal;
