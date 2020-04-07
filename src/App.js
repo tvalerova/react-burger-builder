e5d97ca4a25fd7480d8d9f09d6c7e53cd0524cc3
Tereza Valerova <terezavalerova@gmail.com>
2020-04-07 15:11:02 +0200

    imports changed in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
-  return ( 
+  return (
     <div >
       <Layout>
-        <BurgerBuilder/>
+        <BurgerBuilder />
       </Layout>
-    
+
     </div>
-    );
-  }
-  
-  export default App;
+  );
+}
+
+export default App;
