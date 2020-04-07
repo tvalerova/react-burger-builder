3500680a431e63c5a5118708f67390d541baa8e6
Tereza Valerova <terezavalerova@gmail.com>
2020-04-07 15:12:50 +0200

    imports changed in Modal.js

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import './Modal.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 // the modal will be a component that can be wrapped around any content
