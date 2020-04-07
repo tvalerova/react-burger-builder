18043b2ebf5329e9be7ef8c425ab473751c2bca5
Tereza Valerova <terezavalerova@gmail.com>
2020-04-07 14:43:12 +0200

    Odersummary changed to class-based component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    render() {
+
+        const ingredientSummary = Object.keys(props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
@@ -12,19 +14,20 @@ const orderSummary = (props) => {
                 </li>)
         });
 
-    return (
-        <Aux>
-            <h3>Your order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    )
-};
+        return (
+            <Aux>
+                <h3>Your order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
+}
 
-export default orderSummary;
+export default OrderSummary;
