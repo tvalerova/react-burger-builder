bb50597edb78bfad16dc739a3607dd8d3fe8348e
Tereza Valerova <terezavalerova@gmail.com>
2020-04-07 15:12:25 +0200

    imports changed in Sidedrawer.js

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import styles from './SideDrawer.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 
 const sideDrawer = (props) => {
     // we can close the drawer by clicking on the backdrop
