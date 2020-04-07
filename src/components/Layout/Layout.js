484cc15b237755f8665f7a8dd29c3aac147dd07f
Tereza Valerova <terezavalerova@gmail.com>
2020-04-07 13:17:43 +0200

    sidedrawer basic styling created

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <main className='Content'>
             {/* here we will output the component we wrap with the layout */}
             {props.children}
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import './SideDrawer.css';
 
 const sideDrawer = (props) => {
     return (
-        <div>
+        <div className='SideDrawer'>
             <Logo />
             <nav>
                 <NavigationItems />
