e032fc780436903cec3589ad6adb501bc4db3a04
Tereza Valerova <terezavalerova@gmail.com>
2020-04-07 12:39:30 +0200

    burger icon added to navbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Toolbar.css';
+import Logo from '../../Logo/Logo';
 
 const toolbar = (props) => (
     <header className='Toolbar'>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo />
         <nav>
             ....
         </nav>
