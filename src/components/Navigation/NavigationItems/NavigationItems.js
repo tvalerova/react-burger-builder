cc5c871d50e2acbd2e479c527da2652d0ec21c9d
Tereza Valerova <terezavalerova@gmail.com>
2020-04-07 13:05:25 +0200

    navigation bar links added

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavigationItems.css';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+
+const navigationItems = () => (
+    <ul className='NavigationItems'>
+        {/* active is a boolean, we dont have to say active={true}, it is enough as it is */}
+        <NavigationItem link="/" active>Burger Builder</NavigationItem>
+        <NavigationItem link="/" >Checkout</NavigationItem>
+    </ul>
+);
+
+export default navigationItems;
