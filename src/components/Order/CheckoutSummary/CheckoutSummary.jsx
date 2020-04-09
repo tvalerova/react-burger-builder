f9fc05bb22382f8af5b927dd0f0ef0e6ae8d327b
Tereza Valerova <terezavalerova@gmail.com>
2020-04-09 17:08:07 +0200

    styling created for checkoutsummary.css

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+
+const checkoutSummary = (props) => {
+    return (
+        <div>
+            <h1>We hope it tastes great</h1>
+            <div style={{width: '300px', height: '300px', margin: 'auto'}}>
+                <Burger ingredients={props.ingredients} />
+            </div>
+            <Button btnType="Danger" clicked >CANCEL</Button>
+            <Button btnType="Success" clicked >CONTINUE</Button>
+        </div>
+    )
+}
+
+export default checkoutSummary;
