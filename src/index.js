9ceee24e3455df534a0e60d3c597d9b9cb807d7f
Tereza Valerova <terezavalerova@gmail.com>
2020-04-09 20:02:27 +0200

    correction in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
 
 const app = (
   <BrowserRouter>
@@ -11,12 +12,7 @@ const app = (
   </BrowserRouter>
 );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
