3903e28732fe112dcce30fee2ef9ad11121cf4bd
Tereza Valerova <terezavalerova@gmail.com>
2020-04-09 17:08:55 +0200

    checkoutsummary.jsx created

--- src/components/Order/CheckoutSummary/CheckoutSummary.jsx
+++ src/components/Order/CheckoutSummary/CheckoutSummary.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Burger from '../../Burger/Burger';
 import Button from '../../UI/Button/Button';
+import from './CheckoutSummary.css';
 
 const checkoutSummary = (props) => {
     return (
-        <div>
+        <div className='CheckouSummary'>
             <h1>We hope it tastes great</h1>
             <div style={{width: '300px', height: '300px', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients} />
