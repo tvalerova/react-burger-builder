37861a88a2854ccd52a507bd91a2826d255caa0c
Tereza Valerova <terezavalerova@gmail.com>
2020-04-09 19:43:08 +0200

    browser router set up in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+
+const app = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 ReactDOM.render(
   <React.StrictMode>
