2643469c2bb20f2db41403f1598500d58b63e7ad
Tereza Valerova <terezavalerova@gmail.com>
2020-04-10 11:05:17 +0200

    contact data form created

---
+++ src/containers/Checkout/ContactData/ContactData.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import Button from '../../../components/UI/Button/Button';
+class ContactData extends Component {
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: ''
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                <h4>Contact Information</h4>
+                <form>
+                    <input type="text" name="name" placeholder="Your name" />
+                    <input type="email" name="email" placeholder="Your email" />
+                    <input type="text" name="street" placeholder="Street name" />
+                    <input type="text" name="zipcode" placeholder="Zipcode" />
+                    <Button btnType="Success">ORDER</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default ContactData;
