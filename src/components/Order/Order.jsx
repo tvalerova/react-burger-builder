46332a5a242b66364d364b693b122930aec18f47
Tereza Valerova <terezavalerova@gmail.com>
2020-04-10 13:33:43 +0200

    order component created

---
+++ src/components/Order/Order.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const order = (props) => (
+    <div>
+        <p>Ingredients: </p>
+        <p>Price: <strong>USD </strong></p>
+    </div>
+);
+
+export default order;
