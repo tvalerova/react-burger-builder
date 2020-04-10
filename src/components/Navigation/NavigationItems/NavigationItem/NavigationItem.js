8f07f276f4c02d303cf26fabc4afa3fe74a81ff0
Tereza Valerova <terezavalerova@gmail.com>
2020-04-10 13:47:59 +0200

    link changed to NavLink

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './NavigationItem.css';
+import { NavLink } from 'react-router-dom';
 
 const navigationItem = (props) => (
     <li className='NavigationItem'>
-        <a
-            href={props.link}
-            // if the prop is active, we will give it class active
-            className={props.active ? 'active' : null} >
+        <NavLink
+            to={props.link}>
+            {/* if the prop is active, we will give it class active
+            since we changed this to navlink, we don;t need to add the class, it will be done automatically
+            className={props.active ? 'active' : null}  */}
             {props.children}
-        </a>
+        </NavLink>
     </li>
 );
 
