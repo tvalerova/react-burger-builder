c93f165fa7e8472f2c57ea39cf41275f0bbf38d7
Tereza Valerova <terezavalerova@gmail.com>
2020-04-10 17:34:10 +0200

    input adjusted

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -4,18 +4,26 @@ import './Input.css';
 const input = (props) => {
 
     let inputElement = null;
-    
+
     // we create the form inputs dynamically
-    // for some reason in React 16 and higher when we pass inputType, it throws a warning, so we changed it to inputtype
-    switch (props.inputtype) {
+    switch (props.elementType) {
         case ('input'):
-            inputElement = <input className='InputElement' {...props} />;
+            inputElement = <input
+                className='InputElement'
+                {...props.elementConfig}
+                value={props.value} />;
             break;
         case ('textarea'):
-            inputElement = <textarea className='InputElement' {...props} />;
+            inputElement = <textarea
+                className='InputElement'
+                {...props.elementConfig}
+                value={props.value} />;
             break;
         default:
-            inputElement = <input className='InputElement' {...props} />;
+            inputElement = <input
+                className='InputElement'
+                {...props.elementConfig}
+                value={props.value} />;
     }
 
     return (
