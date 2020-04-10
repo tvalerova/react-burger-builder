1e6e2154d972a9ec0e72f0e73b901c81e30e09d1
Tereza Valerova <terezavalerova@gmail.com>
2020-04-10 13:57:57 +0200

    exact added to root path

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = () => (
     <ul className='NavigationItems'>
         {/* active is a boolean, we dont have to say active={true}, it is enough as it is */}
-        <NavigationItem link="/">Burger Builder</NavigationItem>
+        <NavigationItem link="/" exact >Burger Builder</NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
 );
