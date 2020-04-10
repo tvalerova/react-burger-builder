c1c0a52864ae5a5401121e7007465b325b144814
Tereza Valerova <terezavalerova@gmail.com>
2020-04-10 13:16:19 +0200

    spinner set up when form loads

--- src/containers/Checkout/ContactData/ContactData.jsx
+++ src/containers/Checkout/ContactData/ContactData.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Button from '../../../components/UI/Button/Button';
 import './ContactData.css';
 import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class ContactData extends Component {
     state = {
@@ -49,17 +50,24 @@ class ContactData extends Component {
             });
     }
 
-    render () {
+    // we set up a spinner first when it's loading, then the form
+    render() {
+        let form = (
+            <form>
+                <input className='Input' type="text" name="name" placeholder="Your name" />
+                <input className='Input' type="email" name="email" placeholder="Your email" />
+                <input className='Input' type="text" name="street" placeholder="Street name" />
+                <input className='Input' type="text" name="zipcode" placeholder="Zipcode" />
+                <Button btnType="Success" clicked={this.orderHandler} >ORDER</Button>
+            </form>
+        );
+        if (this.state.loading) {
+            form = <Spinner />;
+        }
         return (
             <div className='ContactData' >
                 <h4>Contact Information</h4>
-                <form>
-                    <input className='Input' type="text" name="name" placeholder="Your name" />
-                    <input className='Input' type="email" name="email" placeholder="Your email" />
-                    <input className='Input' type="text" name="street" placeholder="Street name" />
-                    <input className='Input' type="text" name="zipcode" placeholder="Zipcode" />
-                    <Button btnType="Success" clicked={this.orderHandler} >ORDER</Button>
-                </form>
+                {form}
             </div>
         );
     }
