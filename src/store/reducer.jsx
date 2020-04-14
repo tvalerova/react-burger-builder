4599b03abf28ecce44decee305824c821359e606
Tereza Valerova <terezavalerova@gmail.com>
2020-04-14 16:40:18 +0200

    remove ingredient dispatch action created

--- src/store/reducer.jsx
+++ src/store/reducer.jsx
@@ -3,7 +3,7 @@ import * as actionTypes from './actions';
 const initialState = {
     // we copied this from burgerbuilder
     ingredients: {
-        salad: 0, 
+        salad: 0,
         bacon: 0,
         cheese: 0,
         meat: 0
@@ -30,7 +30,11 @@ const reducer = (state = initialState, action) => {
             };
         case actionTypes.REMOVE_INGREDIENT:
             return {
-
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                }
             };
         default:
             return state;
