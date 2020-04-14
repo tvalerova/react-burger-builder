cec860b20b1928d412942e4d07bd29da73b5fed1
Tereza Valerova <terezavalerova@gmail.com>
2020-04-14 16:27:42 +0200

    switch statement added

--- src/store/reducer.jsx
+++ src/store/reducer.jsx
@@ -7,7 +7,18 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                
+            };
+        case actionTypes.REMOVE_INGREDIENT:
+            return {
 
+            };
+        default:
+            return state;
+    }
 };
 
 export default reducer;
