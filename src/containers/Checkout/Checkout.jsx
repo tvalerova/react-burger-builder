6ab6cad64bf944ddeec4c4d9b8d9e74b7ea33825
Tereza Valerova <terezavalerova@gmail.com>
2020-04-15 11:04:25 +0200

    checkout.jsx adjusted

--- src/containers/Checkout/Checkout.jsx
+++ src/containers/Checkout/Checkout.jsx
@@ -52,17 +52,18 @@ class Checkout extends Component {
         return (
             <div>
                 <CheckoutSummary
-                    ingredients={this.state.ingredients}
+                    ingredients={this.props.ings}
                     checkoutCanceled={this.checkoutCanceledHandler}
                     checkoutContinued={this.checkoutContinuedHandler} />
                 <Route
                     path={this.props.match.path + '/contact-data'}
                     // when we pass the ContacData into render and not as component, we can pass props to it
-                    render={(props) => (<ContactData ingredients={this.state.ingredients}
-                        // because we are rendering the contact data component manually and not through the component prop
-                        // we don't have access to the props objects such as history and location - we therefore passed it to render
-                        // and distribute the with the spread operator - then the push method in ContactData will work and after clicking Order we will be redirected to homepage
-                        price={this.state.totalPrice} {...props} />)} />
+                    // render={(props) => (<ContactData ingredients={this.state.ingredients}
+                    //     // because we are rendering the contact data component manually and not through the component prop
+                    //     // we don't have access to the props objects such as history and location - we therefore passed it to render
+                    //     // and distribute the with the spread operator - then the push method in ContactData will work and after clicking Order we will be redirected to homepage
+                    //     price={this.state.totalPrice} {...props} />)} 
+                    component={ContactData} />
             </div>
         )
     }
@@ -72,7 +73,6 @@ class Checkout extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
     }
 };
 
