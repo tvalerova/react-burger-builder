6f2c72a027effab397bc372f7eac4d876b8dcb40
Tereza Valerova <terezavalerova@gmail.com>
2020-04-15 16:41:04 +0200

    action creator for burger builder created

---
+++ src/store/actions/burgerBuilder.jsx
@@ -0,0 +1,16 @@
+import * as actionTypes from './actionTypes';
+
+// we name our action creators the same as the identifiers, only in cameCase
+export const addIngredient = (name) => {
+    return {
+        type: actionTypes.ADD_INGREDIENT,
+        ingredientName: name
+    }
+}
+
+export const removeIngredient = (name) => {
+    return {
+        type: actionTypes.REMOVE_INGREDIENT,
+        ingredientName: name
+    }
+}
