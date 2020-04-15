4dfb55274b30d48e8b827b25c6d32c594c330fc5
Tereza Valerova <terezavalerova@gmail.com>
2020-04-15 10:32:13 +0200

    price subtraction updated

--- src/store/reducer.jsx
+++ src/store/reducer.jsx
@@ -43,7 +43,8 @@ const reducer = (state = initialState, action) => {
                 ingredients: {
                     ...state.ingredients,
                     [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                }
+                },
+                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
             };
         default:
             return state;
