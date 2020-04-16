aeda25cba01efe04e0af42aad18e3d2ab9c423af
Tereza Valerova <terezavalerova@gmail.com>
2020-04-16 15:46:51 +0200

    form dynamically rendered

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 
@@ -37,11 +37,35 @@ class Auth extends Component {
         }
     }
 
-    render () {
+    render() {
+        const formElementsArray = [];
+        // we covert the oderForm object into an array
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
+        const form = formElementsArray.map(formElement => (
+            <Input
+                key={formElement.id}
+                elementType={formElement.config.elementType}
+                elementConfig={formElement.config.elementConfig}
+                value={formElement.config.value}
+                invalid={!formElement.config.valid}
+                // if we did not set up a validation on an input, this will be false - the red class will therefore not be implemented on it
+                shouldValidate={formElement.config.validation}
+                touched={formElement.config.touched}
+                // the id here is the key from out objects
+                changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+        ));
+
         return (
             <div>
                 <form>
-
+                    {form}
+                    <Button btnType='Success'>SUBMIT</Button>
                 </form>
             </div>
         )
