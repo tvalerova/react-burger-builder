bc385885e24bf394c987ce5a9c5305eed4566e95
Tereza Valerova <terezavalerova@gmail.com>
2020-04-16 16:07:22 +0200

    event connected to component

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -68,8 +68,18 @@ class Auth extends Component {
         return isValid;
     }
 
-    
-
+    inputChangedHandler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                touched: true
+            }
+        };
+        this.setState({controls: updatedControls});
+    }
 
     render() {
         const formElementsArray = [];
