f78edd45958d79d41084c00347b7f25636eef1be
Tereza Valerova <terezavalerova@gmail.com>
2020-04-16 14:13:45 +0200

    burgerBuilder reducer code refactored

--- src/store/reducers/burgerBuilder.jsx
+++ src/store/reducers/burgerBuilder.jsx
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility';
 
 const initialState = {
     // we copied this from burgerbuilder
@@ -20,30 +21,21 @@ const INGREDIENT_PRICES = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    // a special syntax to dynamically overwrite a property in a given JS object
-                    // the square brackets do not create an array here - we pass it a vaariable or something which contains the name we actually want to use as a property name
-                    // we expect to get the property on the action, and call it ingredientName
-                    // ingredient name is something we get as payload to the action
-                    // in here we will receive the new value for an ingredient
-
-                    // we fetch the old state and add 1, and we overwrite the old state with the updated value
-                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                },
+            const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 };
+            const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+            const updatedState = {
+                ingredients: updatedIngredients,
                 totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            };
+            }
+            return updateObject(state, updatedState);
         case actionTypes.REMOVE_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                },
-                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
-            };
+            const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 };
+            const updatedIngs = updateObject(state.ingredients, updatedIngredient);
+            const updatedSt = {
+                ingredients: updatedIngredients,
+                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+            }
+            return updateObject(state, updatedState);
         case actionTypes.SET_INGREDIENT:
             return {
                 ...state,
