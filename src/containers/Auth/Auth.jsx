e736e9b6eaf28dbfd51223900cc9b5673822fa60
Tereza Valerova <terezavalerova@gmail.com>
2020-04-16 18:08:48 +0200

    error message added

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -125,8 +125,17 @@ class Auth extends Component {
             form = <Spinner />
         }
 
+        let errorMessage = null;
+        if (this.props.error) {
+            errorMessage = (
+                // an error message provided by firebase
+                <p>{this.props.error.message}</p>
+            )
+        }
+
         return (
             <div className='Auth' >
+                {errorMessage}
                 <form onSubmit={this.submitHandler} >
                     {form}
                     <Button btnType='Success'>SUBMIT</Button>
@@ -142,7 +151,8 @@ class Auth extends Component {
 
 const mapStateToProps = state => {
     return {
-        loading: state.auth.loading
+        loading: state.auth.loading,
+        error: state.auth.error
     }
 }
 
