e9114c8775961b015e3b4251cd5a5fe3ebc05693
Tereza Valerova <terezavalerova@gmail.com>
2020-04-16 18:19:10 +0200

    logout and setTimeout adctions added

--- src/store/actions/auth.jsx
+++ src/store/actions/auth.jsx
@@ -22,6 +22,20 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime);
+    };
+};
+
 export const auth = (email, password, isSignedUp) => {
     return dispatch => {
         dispatch(authStart());
@@ -38,6 +52,7 @@ export const auth = (email, password, isSignedUp) => {
         .then(response => {
             console.log(response);
             dispatch(authSuccess(response.data.idToken, response.data.localId));
+            dispatch(checkAuthTimeout(response.data.expiresIn));
         })
         .catch(err => {
             console.log(err);
