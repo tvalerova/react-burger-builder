e009680139a22f4c76056c045e3d0e3cccfdfcf7
Tereza Valerova <terezavalerova@gmail.com>
2020-04-16 14:47:08 +0200

    errors fixed

--- src/store/reducers/order.jsx
+++ src/store/reducers/order.jsx
@@ -50,7 +50,7 @@ const reducer = (state = initialState, action) => {
         case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action);
         case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerfail(state, action);
         case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state, action);
-        case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccessstate, action);
+        case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state, action);
         case actionTypes.FETCH_ORDERS_FAIL: return fetchOrdersFail(state, action);
         default: return state;
     }
