a9734fbb490e9a7944d299de520b731efc5b363c
Tereza Valerova <terezavalerova@gmail.com>
2020-04-16 13:44:36 +0200

    more case statements added

--- src/store/reducers/order.jsx
+++ src/store/reducers/order.jsx
@@ -9,15 +9,15 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     swtich(action.type) {
         case actionTypes.PURCHASE_INIT:
-            return {
-                ...state,
-                purchased: false
-            }
+return {
+    ...state,
+    purchased: false
+}
         case actionTypes.PURCHASE_BURGER_START:
-            return {
-                ...state,
-                loading: true
-            };
+return {
+    ...state,
+    loading: true
+};
         case actionTypes.PURCHASE_BURGER_SUCCESS:
 const newOrder = {
     ...action.orderData,
@@ -28,13 +28,29 @@ return {
     loading: false,
     purchased: true,
     orders: state.orders.concat(newOrder),
-    
+
 };
             case actionTypes.PURCHASE_BURGER_SUCCESS:
 return {
     ...state,
     loading: false
 };
+case actionTypes.FETCH_ORDERS_START:
+return {
+    ...state,
+    loading: true
+};
+case actionTypes.FETCH_ORDERS_SUCCESS:
+    return {
+        ...state, 
+        orders: action.orders, 
+        loading: false
+    };
+    case actionTypes.FETCH_ORDERS_FAIL:
+        return {
+            ...state, 
+            loading: false
+        };
                 default:
 return state;
     }
