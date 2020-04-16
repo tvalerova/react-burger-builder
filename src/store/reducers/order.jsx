d3d4332c9747e98095b75b86caa51b2c7f622d40
Tereza Valerova <terezavalerova@gmail.com>
2020-04-16 12:10:24 +0200

    order js reducer set up

---
+++ src/store/reducers/order.jsx
@@ -0,0 +1,29 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+    orders: [],
+    loading: false
+}
+
+const reducer = (state = initialState, action) => {
+    swtich (action.type) {
+        case actionTypes.PURCHASE_BURGER_SUCCESS:
+            const newOrder = {
+                ...action.orderData,
+                id: action.orderId
+            }
+
+            return {
+                ...state,
+                loading: false,
+                orders: state.orders.concat(newOrder)
+            };
+            case actionTypes.PURCHASE_BURGER_SUCCESS:
+                return {
+                    ...state,
+                    loading: false
+                };
+                default: 
+                return state;
+    }
+}
