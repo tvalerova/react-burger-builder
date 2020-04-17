a08bd7cba4661c1801a0dbcc1a5861ece3b402c1
Tereza Valerova <terezavalerova@gmail.com>
2020-04-17 13:27:25 +0200

    redirecting set up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
@@ -15,11 +15,11 @@ class App extends Component {
   }
 
   render() {
-
     let routes = (
       <Switch>
         <Route path="/auth" component={Auth} />
         <Route path="/" component={BurgerBuilder} />
+        <Redirect to="/" />
       </Switch>
     );
 
@@ -30,6 +30,7 @@ class App extends Component {
           <Route path="/orders" component={Orders} />
           <Route path="/logout" component={Logout} />
           <Route path="/" component={BurgerBuilder} />
+          <Redirect to="/" />
         </Switch>
       )
     }
