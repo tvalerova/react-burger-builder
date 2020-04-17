ff78e35dee7608fff58259d68c31714f53583b42
Tereza Valerova <terezavalerova@gmail.com>
2020-04-17 11:39:31 +0200

    orders hidden when user is not authenticated

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,7 +7,7 @@ const navigationItems = (props) => (
     <ul className='NavigationItems'>
         {/* active is a boolean, we dont have to say active={true}, it is enough as it is */}
         <NavigationItem link="/" exact >Burger Builder</NavigationItem>
-        <NavigationItem link="/orders">Orders</NavigationItem>
+        {props.isAuthenticated ? <NavigationItem link="/orders">Orders</NavigationItem> : null }
         {!props.isAuthenticated
             ? <NavigationItem link="/auth">Log In</NavigationItem>
             : <NavigationItem link="/logout">Log Out</NavigationItem>}
