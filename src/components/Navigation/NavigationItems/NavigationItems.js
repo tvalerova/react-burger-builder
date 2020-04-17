48e2bbadccadade51b0a2d6785347aee89b4b239
Tereza Valerova <terezavalerova@gmail.com>
2020-04-17 10:52:15 +0200

    logic set up to either show log in or logout

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,12 +3,14 @@ import './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
 
-const navigationItems = () => (
+const navigationItems = (props) => (
     <ul className='NavigationItems'>
         {/* active is a boolean, we dont have to say active={true}, it is enough as it is */}
         <NavigationItem link="/" exact >Burger Builder</NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
-        <NavigationItem link="/auth">Log In</NavigationItem>
+        {!props.isAuthenticated
+            ? <NavigationItem link="/auth">Log In</NavigationItem>
+            : <NavigationItem link="/logout">Log Out</NavigationItem>}
     </ul>
 );
 
