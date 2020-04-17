1c12f19a24eb0134f162544400bd2a7e8062efc0
Tereza Valerova <terezavalerova@gmail.com>
2020-04-17 11:44:34 +0200

    redirect after authentication

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -5,7 +5,7 @@ import './Auth.css';
 import * as actions from '../../store/actions/index';
 import { connect } from 'react-redux';
 import Spinner from '../../components/UI/Spinner/Spinner';
-
+import {Redirect} from 'react-router-dom';
 
 class Auth extends Component {
     state = {
@@ -130,11 +130,17 @@ class Auth extends Component {
             errorMessage = (
                 // an error message provided by firebase
                 <p>{this.props.error.message}</p>
-            )
+            );
+        }
+
+        let authRedirect = null;
+        if (this.props.isAuthenticated) {
+            authRedirect = <Redirect to='/' />
         }
 
         return (
             <div className='Auth' >
+                {authRedirect}
                 {errorMessage}
                 <form onSubmit={this.submitHandler} >
                     {form}
@@ -152,7 +158,8 @@ class Auth extends Component {
 const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
-        error: state.auth.error
+        error: state.auth.error,
+        isAuthenticated: state.auth.token !== null
     }
 }
 
