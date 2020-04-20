c6fb512bedc1a343ae31c3705fb26028e16dfc1f
Tereza Valerova <terezavalerova@gmail.com>
2020-04-20 10:46:48 +0200

    sidedrawer closing error fixed

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -16,7 +16,7 @@ const sideDrawer = (props) => {
         <Aux>
             {/* backdrop will alway show since show = true */}
             <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={attachedClasses.join(' ')}>
+            <div className={attachedClasses.join(' ')} onClick={props.closed}>
                 <div className={styles.Logo}>
                     <Logo />
                 </div>
