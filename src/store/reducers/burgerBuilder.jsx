00a28e16bdf01531fd634001f0acc58d2414ae5e
Tereza Valerova <terezavalerova@gmail.com>
2020-04-20 10:50:35 +0200

    utility.js moved to shared folder

--- src/store/reducers/burgerBuilder.jsx
+++ src/store/reducers/burgerBuilder.jsx
@@ -1,5 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
-import { updateObject } from '../utility';
+import { updateObject } from '../../share/utility';
 import { setIngredients } from '../actions/burgerBuilder';
 
 const initialState = {
