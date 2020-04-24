5f70068a44361f48bd4f30e4a31730de99be4ac7
Tereza Valerova <terezavalerova@gmail.com>
2020-04-24 13:48:56 +0200

    saga middleware added to index.js

--- src/store/sagas/auth.jsx
+++ src/store/sagas/auth.jsx
@@ -2,7 +2,7 @@ import {put} from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
 
 // we are doing the same here as we did in the auth LOGOUT action
-function* logout(action) {
+export function* logoutSaga(action) {
     yield localStorage.removeItem('token');
     yield localStorage.removeItem('expirationDate');
     yield localStorage.removeItem('userId');
