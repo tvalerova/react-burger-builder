2f1af961e471f1650fa8667fb16c1e94af3c4311
Tereza Valerova <terezavalerova@gmail.com>
2020-04-24 14:17:58 +0200

    new generator in auth saga

--- src/store/sagas/auth.jsx
+++ src/store/sagas/auth.jsx
@@ -1,6 +1,8 @@
+import {delay} from 'redux-saga';
 import {put} from 'redux-saga/effects';
 
 import * as actions from '../actions/index';
+import { func } from 'prop-types';
 
 // we are doing the same here as we did in the auth LOGOUT action
 export function* logoutSaga(action) {
@@ -9,3 +11,8 @@ export function* logoutSaga(action) {
     yield localStorage.removeItem('userId');
     yield put(actions.logoutSucceed());
 };
+
+export function* checkAuthTimeoutSaga(action) {
+    yield delay(action.expirationTime);
+    yield put(actions.logout());
+}
