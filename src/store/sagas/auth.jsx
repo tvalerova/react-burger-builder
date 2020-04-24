9b893d6eacedcb45b0ed0c3e9d963ea279900e8c
Tereza Valerova <terezavalerova@gmail.com>
2020-04-24 17:14:11 +0200

    new saga created

--- src/store/sagas/auth.jsx
+++ src/store/sagas/auth.jsx
@@ -1,5 +1,6 @@
 import {delay} from 'redux-saga';
 import {put} from 'redux-saga/effects';
+import axios from 'axios';
 
 import * as actions from '../actions/index';
 import { func } from 'prop-types';
@@ -15,4 +16,30 @@ export function* logoutSaga(action) {
 export function* checkAuthTimeoutSaga(action) {
     yield delay(action.expirationTime);
     yield put(actions.logout());
+}
+
+export function* authUserSaga(action) {
+    yield put(actions.authStart());
+    const authData = {
+            email: action.email,
+            password: action.password,
+            returnSecureToken: true
+        };
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDFX2aTHI1c4exk7g7ecMn8VYOfj_8Pxms';
+        if (!action.isSignUp) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDFX2aTHI1c4exk7g7ecMn8VYOfj_8Pxms';
+        }
+        axios.post(url, authData)
+            .then(response => {
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                // localStorage is built into the browser, it will store the token
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
+            })
+            .catch(err => {
+                dispatch(authFail(err.response.data.error));
+            });
 }
