2ea268cd62715da241a7a93d83b0bef48eb0cb8f
Tereza Valerova <terezavalerova@gmail.com>
2020-04-24 14:09:21 +0200

    new action type created

--- src/store/actions/auth.jsx
+++ src/store/actions/auth.jsx
@@ -31,6 +31,12 @@ export const logout = () => {
     };
 };
 
+export const logoutSucceed = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+
 export const checkAuthTimeout = (expirationTime) => {
     return dispatch => {
         setTimeout(() => {
