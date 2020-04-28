a8bd18700c31c5c7ec5d3253aaeb6adf357aa5e1
Tereza Valerova <terezavalerova@gmail.com>
2020-04-28 12:49:21 +0200

    auth user saga created

--- src/store/sagas/auth.jsx
+++ src/store/sagas/auth.jsx
@@ -1,5 +1,5 @@
-import {delay} from 'redux-saga';
-import {put} from 'redux-saga/effects';
+import { delay } from 'redux-saga';
+import { put } from 'redux-saga/effects';
 import axios from 'axios';
 
 import * as actions from '../actions/index';
@@ -21,25 +21,25 @@ export function* checkAuthTimeoutSaga(action) {
 export function* authUserSaga(action) {
     yield put(actions.authStart());
     const authData = {
-            email: action.email,
-            password: action.password,
-            returnSecureToken: true
-        };
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDFX2aTHI1c4exk7g7ecMn8VYOfj_8Pxms';
-        if (!action.isSignUp) {
-            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDFX2aTHI1c4exk7g7ecMn8VYOfj_8Pxms';
-        }
-        axios.post(url, authData)
-            .then(response => {
-                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
-                // localStorage is built into the browser, it will store the token
-                localStorage.setItem('token', response.data.idToken);
-                localStorage.setItem('expirationDate', expirationDate);
-                localStorage.setItem('userId', response.data.localId);
-                dispatch(authSuccess(response.data.idToken, response.data.localId));
-                dispatch(checkAuthTimeout(response.data.expiresIn));
-            })
-            .catch(err => {
-                dispatch(authFail(err.response.data.error));
-            });
+        email: action.email,
+        password: action.password,
+        returnSecureToken: true
+    };
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDFX2aTHI1c4exk7g7ecMn8VYOfj_8Pxms';
+    if (!action.isSignUp) {
+        url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDFX2aTHI1c4exk7g7ecMn8VYOfj_8Pxms';
+    }
+    try {
+        const response = yield axios.post(url, authData)
+
+        const expirationDate = yield new Date(new Date().getTime() + response.data.expiresIn * 1000);
+        // localStorage is built into the browser, it will store the token
+        yield localStorage.setItem('token', response.data.idToken);
+        yield localStorage.setItem('expirationDate', expirationDate);
+        yield localStorage.setItem('userId', response.data.localId);
+        yield put(actions.authSuccess(response.data.idToken, response.data.localId));
+        yield put(actions.checkAuthTimeout(response.data.expiresIn));
+    } catch (error) {
+        yield put(actions.authFail(error.response.data.error));
+    }
 }
